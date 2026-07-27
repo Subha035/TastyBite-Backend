2d7947547d28cfebb07773fd6d80a4e4b5f0e085
Subhadeep Ghosh <138667432+Subha035@users.noreply.github.com>
2026-07-28 01:27:44 +0530

    Update FirebaseConfig.java

--- src/main/java/com/example/backend/config/FirebaseConfig.java
+++ src/main/java/com/example/backend/config/FirebaseConfig.java
@@ -51,6 +51,7 @@ public class FirebaseConfig {
             return FirestoreClient.getFirestore();
 
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("Failed to initialize Firebase", e);
         }
     }
